b5e4ab6c38c2485651dbd62f33aa1ce4128f3267
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2022-08-13 20:40:16 -0700

    Fixes, optimizations and features improvements - Optimized memory consumption - Implemented sort feature - Other small bug fixes and improvements

--- main.cpp
+++ main.cpp
@@ -2,37 +2,28 @@
 #include <fstream>
 #include <string>
 #include <map>
-#include <fstream>
+#include <unordered_map>
+#include <vector>
+#include "algorithm"
 
-const int SUPPOSED_PARAMETERS_COUNT = 5;
-enum class DataTypes {
-    INTEGER,
-    STRING,
-    FLOAT
-};
-enum class SortMethods {
-    ASCENDING,
-    DESCENDING
-};
 
-std::map<std::string , DataTypes> map_key_to_datatype_parameter = {
-        {"i", DataTypes::INTEGER},
-        {"s", DataTypes::STRING},
-        {"f", DataTypes::FLOAT}
-};
-std::map<std::string , SortMethods> map_key_to_sort_method_parameter = {
-        {"a", SortMethods::ASCENDING},
-        {"d", SortMethods::DESCENDING}
-};
+
+
 
 class LaunchParameters {
 public:
+    enum class DataTypes :std::uint8_t {
+        INTEGER,
+        STRING,
+        FLOAT
+    };
+    enum class SortMethods :std::uint8_t{
+        ASCENDING,
+        DESCENDING
+    };
     LaunchParameters(const int& parameters_count, char**& raw_parameters) {
         assert(parameters_count == SUPPOSED_PARAMETERS_COUNT);
-        this->data_type = map_key_to_datatype_parameter[raw_parameters[1]];
-        this->sort_method = map_key_to_sort_method_parameter[raw_parameters[2]];
-        this->input_file = raw_parameters[3];
-        this->output_file = raw_parameters[4];
+        ParseParameters(parameters_count, raw_parameters);
     }
 
     [[nodiscard]] DataTypes getDataType() const {
@@ -50,38 +41,109 @@ public:
     [[nodiscard]] const std::string &getOutputFile() const {
         return output_file;
     }
-
 private:
-    DataTypes data_type;
-    SortMethods sort_method;
-    std::string input_file;
-    std::string output_file;
+    const int SUPPOSED_PARAMETERS_COUNT = 5;
+    const std::unordered_map<std::string , DataTypes> map_key_to_datatype_parameter = {
+            {"i", DataTypes::INTEGER},
+            {"s", DataTypes::STRING},
+            {"f", DataTypes::FLOAT}
+    };
+    const std::unordered_map<std::string , SortMethods> map_key_to_sort_method_parameter = {
+            {"a", SortMethods::ASCENDING},
+            {"d", SortMethods::DESCENDING}
+    };
+
+    DataTypes data_type {};
+    SortMethods sort_method {};
+    std::string input_file {};
+    std::string output_file {};
+    void ParseParameters(const int& parameters_count, char**& raw_parameters) {
+        for(size_t index = 1; index < parameters_count; index++) {
+            auto parameter = raw_parameters[index];
+            if(map_key_to_datatype_parameter.contains(parameter)){
+                data_type = map_key_to_datatype_parameter.at(parameter);
+                continue;
+            }
+            if(map_key_to_sort_method_parameter.contains(parameter)) {
+                sort_method = map_key_to_sort_method_parameter.at(parameter);
+                continue;
+            }
+            if(input_file.empty()) {
+                input_file = parameter;
+                continue;
+            }
+            if(output_file.empty()) {
+                output_file = parameter;
+                continue;
+            }
+        }
+
+    }
+
+
 };
-class FileHandler {
+class FileContentSorter {
 public:
-    FileHandler(std::string file_name) {
-        this->file_name = file_name;
+    explicit FileContentSorter(LaunchParameters& launch_parameters) { /* TODO What does 'explicit' mean?*/
+        this->extract_file_content_to_vector(launch_parameters.getInputFile());
+        this->SortFileContent(launch_parameters.getDataType(),
+                              launch_parameters.getSortMethod());
+        this->save_sorted_content_in_output_file(launch_parameters.getOutputFile());
     }
-    void open_file() {
-        this->opened_file.open(this->file_name, std::fstream::out);
-        std::cout << opened_file.is_open() << std::endl;
+    void extract_file_content_to_vector(auto& input_file_name) {
+        this->opened_file.open(input_file_name, std::fstream::in);
+        if (this->opened_file.is_open()) {
+            std::cout << "OPENED" << std::endl;
+            std::string line;
+            while (getline(this->opened_file, line)) {
+                this->file_content.push_back(line);
+            }
+            this->opened_file.close();
+        }
+        else {
+            std::cout << "Can't open file" << std::endl;
+        }
     }
-    void read_opened_file() {
+    void SortFileContent(const LaunchParameters::DataTypes& data_type,
+                         const LaunchParameters::SortMethods& sort_method) {
+        switch (sort_method) {
+            case (LaunchParameters::SortMethods::ASCENDING):
+                std::sort(this->file_content.begin(), this->file_content.end(),
+                          [](const auto& lhs,const auto& rhs) {
+                              return lhs < rhs;
+                          });
+                break;
+            case LaunchParameters::SortMethods::DESCENDING:
+                std::sort(this->file_content.begin(), this->file_content.end(),
+                          [](const auto& lhs, const auto& rhs) {
+                              return lhs > rhs;
+                          });
+                break;
+            default:
+                assert("Unknown search method");
+                break;
+        }
 
     }
-
+    void save_sorted_content_in_output_file(const std::string& output_file_name) {
+        opened_file.open(output_file_name, std::fstream::out);
+        if(opened_file.is_open()) {
+            for(const auto& line : this->file_content) {
+                opened_file << line << '\n';
+            }
+            opened_file.close();
+        } else {
+            std::cout << "File isn't opened" << std::endl;
+        }
+    }
 
 private:
-    std::string file_name;
     std::fstream opened_file;
-    char** lines;
+    std::vector<std::string> file_content;
 };
 
 int main(int argc, char** argv) {
     LaunchParameters launch_parameters(argc, argv);
-
-    FileHandler input_file(launch_parameters.getInputFile());
-    input_file.open_file();
-
+    FileContentSorter file_sorter(launch_parameters);
     return 0;
 }
